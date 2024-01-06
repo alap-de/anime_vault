08dba687f1b347799878b1c697badb7b02575095
Alap <61322030+alap-de@users.noreply.github.com>
2024-01-06 12:30:49 +0530

    Add try catch

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,37 +8,43 @@ interface FetchAnimeFilters {
   order?: "popularity" | "ranked";
 }
 const MAX_LIMIT = 8;
+const query = `
+query getAnimes($page: Int, $order: OrderEnum, $limit: Int) {
+  animes(page: $page, order: $order, limit: $limit) {
+    id
+    name
+    kind
+    score
+    episodes
+    episodesAired
+    poster { mainUrl }
+  }
+}
+`;
+
 export async function fetchAnime({
   page,
   order = "popularity",
 }: FetchAnimeFilters) {
-  const query = `
-  query getAnimes($page: Int, $order: OrderEnum, $limit: Int) {
-    animes(page: $page, order: $order, limit: $limit) {
-      id
-      name
-      kind
-      score
-      episodes
-      episodesAired
-      poster { mainUrl }
+  try {
+    const response = await fetch(`${BASE_URL}/api/graphql`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query,
+        variables: { page, order, limit: MAX_LIMIT },
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
+
+    const data: AnimeProp[] = (await response.json()).data.animes;
+
+    return data;
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
-  `;
-  const response = await fetch(`${BASE_URL}/api/graphql`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query,
-      variables: { page, order, limit: MAX_LIMIT },
-    }),
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const data: AnimeProp[] = (await response.json()).data.animes;
-
-  return data;
 }
